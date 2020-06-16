e7e0be6f1901a10c67620926dea8a6cffbc78fa2
angganova2019 <angga24.opa@gmail.com>
2020-06-16 13:32:05 +0700

    first Commit

---
+++ src/test/java/com/angga/training/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angga.training.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
