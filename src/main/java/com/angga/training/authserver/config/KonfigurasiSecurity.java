0567cca0c68529ae546889fc62fe11d37659c4ae
angganova2019 <angga24.opa@gmail.com>
2020-06-20 12:17:10 +0700

    third commit

---
+++ src/main/java/com/angga/training/authserver/config/KonfigurasiSecurity.java
@@ -0,0 +1,58 @@
+package com.angga.training.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+
+    private static final String SQL_LOGIN = "select u.username, up.password, u.active " +
+            "from s_users_passwords up " +
+            "inner join s_users u on u.id = up.id_user " +
+            "where u.username = ?";
+    private static final String SQL_PERMISSION = "select u.username, p.value as authority " +
+            "from s_users u " +
+            "inner join s_roles r on r.id = u.id_role " +
+            "inner join s_roles_permissions rp on rp.id_role = r.id " +
+            "inner join s_permissions p on rp.id_permission = p.id " +
+            "where u.username = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(bcryptEncoder());
+    }
+
+    @Bean
+    private PasswordEncoder bcryptEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(this.dataSource);
+        manager.setUsersByUsernameQuery(SQL_LOGIN);
+        manager.setAuthoritiesByUsernameQuery(SQL_PERMISSION);
+        return manager;
+    }
+}
